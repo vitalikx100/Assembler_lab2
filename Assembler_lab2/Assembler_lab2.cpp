481ef29b4c26007d66a992c8968b2e30ff095b01
vitalikx100 <vitalikstarigin@mail.ru>
2023-04-17 22:24:26 +0400

    Добавьте файлы проекта.

---
+++ Assembler_lab2/Assembler_lab2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int calc(int a, int b, int d, double s)
+{
+	_asm
+	{
+		mov eax, a
+		mov ebx, b
+		mov ecx, d
+
+		; вычисление второй скобки
+
+		imul eax, ebx; <edx:eax> = a * b
+		inc eax; <eax> = a * b + 1
+		adc edx, 0
+		; jo error_of
+
+		push eax; в стеке a* b + 1
+
+		; вычисление первой скобки
+
+		mov eax, a
+		imul eax, -4; <edx:eax> = -4 * a
+		add eax, ecx; <eax> = -4 * a + d
+		mov ?, -153;
+		cdq
+		idiv ebx
+		add eax, ? 
+
+
+		
+
+	}
+
+}
+
+int main()
+{
+
+	//(-153/b + d-4*a)/(1 +a*b);
+	int a, b, d, s;
+	s = 0;
+	cin >> a >> b >> d;
+	calc(a, b, d, s);
+	cout << s;
+	return 0;
+
+
+}
